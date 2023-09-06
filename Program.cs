7001176c84dfe63a3b617de2c7e56b58a4b26b9a
WebDev <leandranm@outlook.com>
2023-09-06 14:37:45 +0200

    Complete - Stage 1, Program is Fully Functional

--- Program.cs
+++ Program.cs
@@ -1,8 +1,41 @@
 ﻿namespace GuessingGame;
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        bool isCorrectGuess = false;
+        Random random = new Random(); // Generates random numbers
+
+        int randomNum = random.Next(1, 11); // Stores the generated random number
+
+        Console.WriteLine("Welcome to the number guessing game!");
+        Console.WriteLine("A number between 1 and 10 will be generated");
+        Console.WriteLine("If you guess the correct number you win!\n");
+
+        while (!isCorrectGuess)
+        {
+            Console.Write("Enter your guess.");
+            int guess = Convert.ToInt32(Console.ReadLine());
+
+            if (guess > randomNum)
+            {
+                Console.WriteLine("Your guess is too high!");
+            }
+            else if (guess < randomNum)
+            {
+                Console.WriteLine("Your guess is too low!");
+            }
+            else
+            {
+                Console.WriteLine("Correct!");
+                isCorrectGuess = true;
+            }
+        }
+
+        Console.WriteLine("Congratulations, you have won the game!");
+
+        // Makes the program wait for user input before the Console closes
+        Console.ReadKey();
     }
 }
